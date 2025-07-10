9b3f129b1d42e3691aa042a83c7acd7dc4e3e658
Nuzhat Firdosh <nuzhatfrd@gmail.com>
2025-07-10 20:10:49 +0530

    Find all non-repeating elements in the array

--- Arrays/prblm14.java
+++ Arrays/prblm14.java
@@ -1,33 +1,36 @@
 package Arrays;
-//Find all Symmetric Pairs in the array of pairs
-import java.util.*;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+//Find all non-repeating elements in an array
 public class prblm14 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of array:-");
         int n = sc.nextInt();
-        int[][] nums = new int[n][2];
-        System.out.println("Enter the pairs of the array:-");
+        int[] nums = new int[n];
+        System.out.println("Enter the numbers of the array:-");
         for(int i = 0 ; i < n ; i++){
-                nums[i][0] = sc.nextInt();
-                nums[i][1] = sc.nextInt();
+            nums[i] = sc.nextInt();
         }
-        symmetricPair(nums);
+        nonRepeating(nums);
         sc.close();
     }
-    private static void symmetricPair(int[][] nums){
-        Map<Integer , Integer> pairs = new HashMap<>();
-        for(int[] pair : nums){
-            int a = pair[0];
-            int b = pair[1];
-            if(pairs.containsKey(b) && pairs.get(b) == a){
-                System.out.println("(" + a + ", " + b + ")");
-            }
-            else{
-                pairs.put(a,b);
+    private static void nonRepeating(int[] nums){
+        int n , i ;
+        n = nums.length;
+        Map<Integer , Integer> result = new HashMap<>();
+        for(i = 0 ; i < n ; i ++){
+            result.put(nums[i] , result.getOrDefault(nums[i] , 0)+1);
+        }
+        System.out.println("The non-repeating elements of the array are:-");
+        for(var num : result.entrySet()){
+            if(num.getValue() == 1){
+                System.out.print(num.getKey() + " ");
             }
         }
     }
 }
-
