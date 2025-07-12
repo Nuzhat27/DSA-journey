65220c832b44a874458275f769261e03692cb2df
Nuzhat Firdosh <nuzhatfrd@gmail.com>
2025-07-12 17:21:49 +0530

    LC 121. Best Time to Buy and Sell Stock

---
+++ Arrays/prblm16.java
@@ -0,0 +1,20 @@
+package Arrays;
+//121. Best Time to Buy and Sell Stock
+public class prblm16 {
+    public int maxProfit(int[] prices) {
+        /*int i , j ;
+        int n = prices.length;
+        for(i = 0 ; i < n ; i ++){
+            for(j = i + 1 ; j < n ; j ++){
+                profit = prices[j] - prices[i];
+                maxProfit = Math.max(profit , maxProfit);
+            }
+        }*/
+        int minPrice=Integer.MAX_VALUE , maxProfit = 0 ;
+        for(int price : prices){
+            minPrice = Math.min(minPrice , price);
+            maxProfit = Math.max(maxProfit , price - minPrice);
+        }
+        return maxProfit;
+    }
+}
