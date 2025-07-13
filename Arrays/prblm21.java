54acbf71f49c8918c27a6d0f522cbc6375e2518c
Nuzhat Firdosh <nuzhatfrd@gmail.com>
2025-07-13 20:53:26 +0530

    LC 66. Plus One

---
+++ Arrays/prblm21.java
@@ -0,0 +1,20 @@
+package Arrays;
+/*LC 66. Plus One
+ You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+Increment the large integer by one and return the resulting array of digits.
+ */
+public class prblm21 {
+    public int[] plusOne(int[] digits) {
+        for(int i = digits.length - 1 ; i >= 0 ; i --){
+            if(digits[i] < 9){
+                digits ++ ;
+                return digits ;
+            }
+            digits[i] = 0 ;
+        }
+        int[] result = new int[digits.length + 1];
+        result[0] = 1 ;
+        return results ;
+    }
+}
