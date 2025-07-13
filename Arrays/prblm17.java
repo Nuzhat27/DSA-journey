dd4e7de14712d0fb3620e1df71ddf2bb6b314cbc
Nuzhat Firdosh <nuzhatfrd@gmail.com>
2025-07-13 17:58:16 +0530

    LC14. Longest Common Prefix

---
+++ Arrays/prblm17.java
@@ -0,0 +1,33 @@
+package Arrays ;
+
+import java.util.Arrays;
+
+//LC14. Longest Common Prefix
+public class prblm17 {
+    public String longestCommonPrefix(String[] strs) {
+        // Sort the array to get the lexicographically smallest and largest strings
+        int n = strs.length;
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[n-1];
+        /*int min = Math.min(first.length() , last.length());
+        String ans = "" ;
+        for(int i = 0 ; i < min ; i ++){
+            if(first.charAt(i) != last.charAt(i)){
+                break;
+            }
+            ans += first.charAt(i);
+        }
+        return ans ;*/
+        int index = 0 ;
+        while(index < first.length()){
+            if(first.charAt(index) == last.charAt(index)){
+                index++ ;
+            }
+            else{
+                break;
+            }
+        }
+        return index == 0 ? "" : first.substring(0 , index);
+    }
+}
