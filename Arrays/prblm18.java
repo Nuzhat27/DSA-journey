7290ab0bca73cf37e76abf72652cdf4ace277ce8
Nuzhat Firdosh <nuzhatfrd@gmail.com>
2025-07-13 18:00:21 +0530

    //27. Remove Element

---
+++ Arrays/prblm18.java
@@ -0,0 +1,15 @@
+package Arrays
+//27. Remove Element
+public class prblm18 {
+    public int removeElement(int[] nums, int val) {
+        int n = nums.length ;
+        int i = 0 ;
+        for(int j = 0 ; j < n ; j ++){
+            if(nums[j] != val){
+                nums[i] = nums[j];
+                i ++ ;
+            }
+        }
+        return i ;
+    }
+}
