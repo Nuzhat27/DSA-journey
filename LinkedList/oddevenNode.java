842ed1b85939a0cffaf831b43fe2a58d811f2f73
Nuzhat Firdosh <nuzhatfrd@gmail.com>
2025-08-28 17:04:21 +0530

    Segregate odd and even nodes of a linkedlist

---
+++ LinkedList/oddevenNode.java
@@ -0,0 +1,28 @@
+package LinkedList;
+/*Segregate odd and even nodes in LL
+Given the head of a singly linked list.
+ Group all the nodes with odd indices followed by all the nodes
+ with even indices and return the reordered list.
+Consider the 1st node to have index 1 and so on. 
+The relative order of the elements inside the odd and even group
+ must remain the same as the given input.
+*/
+
+class oddevenNode{
+	public ListNode segregate(ListNode head){
+		if(head == null || head.next == null)return head;
+		ListNode even = head.next;
+		ListNode evenHead = head.next;
+		ListNode odd = head;
+		while(odd != null || even != null){
+			odd.next = odd.next.next;
+			odd = odd.next;
+			
+			even.next = even.next.next;
+			even = even.next;
+			
+		}
+		odd.next = evenHead;
+		return head;
+	}
+}
