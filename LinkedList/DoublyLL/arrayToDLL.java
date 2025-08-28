30edea44a15a869d5c14a344855f81da7313ee5d
Nuzhat Firdosh <nuzhatfrd@gmail.com>
2025-08-28 16:47:04 +0530

    Convert array to Dll

--- LinkedList/DoublyLL/arrayToDLL.java
+++ LinkedList/DoublyLL/arrayToDLL.java
@@ -1,1 +1,15 @@
 package LinkedList.DoublyLL;
+//Array to Doubly LL
+class arrayToDLL{
+	public ListNode DLL(int[] arr){
+		ListNode head = new ListNode(arr[0]);
+		ListNode temp;
+		ListNode prev = head;
+		for(int i = 1 ; i < arr.length ; i ++){
+			temp = new ListNode(arr[i] , null , prev);
+			prev.next = temp;
+			prev = temp;
+		}
+		return head;
+	}
+}
