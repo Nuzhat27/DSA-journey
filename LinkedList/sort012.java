4d4e735fd5a9425c04f7a2b071f2a3bcb379bfde
Nuzhat Firdosh <nuzhatfrd@gmail.com>
2025-08-28 20:48:38 +0530

    Sort linkedlist of 0's 1's and 2's

---
+++ LinkedList/sort012.java
@@ -0,0 +1,6 @@
+package LinkedList;
+/*Sort a LL of 0's 1's and 2's
+Given the head of a singly linked list consisting of only 0, 1 or 2.
+Sort the given linked list and return the head of the modified list.
+Do it in-place by changing the links between the nodes without creating new nodes.
+*/
