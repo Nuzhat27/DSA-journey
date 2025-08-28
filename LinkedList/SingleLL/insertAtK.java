bf45b0b69d557282539ed64edb51f997126386b5
Nuzhat Firdosh <nuzhatfrd@gmail.com>
2025-08-28 16:47:53 +0530

    Insert at kth position of Sll

--- LinkedList/SingleLL/insertAtK.java
+++ LinkedList/SingleLL/insertAtK.java
@@ -1,1 +1,24 @@
 package LinkedList.SingleLL;
+class insertAtK{
+	public ListNode insert(ListNode head , int X , int k){
+		ListNode temp = head;
+		ListNode newNode = new ListNode(X);
+		if(head == null) return newNode;
+		if(k == 1){
+			newNode.next = head;
+			return newnode;
+		}
+		int count = 0;
+		while(temp!=null){
+			count++;
+			if(count == k-1){
+				newNode.next = newNode;
+				temp.next = newNode;
+				break;
+			}
+			
+			temp = temp.next;
+		}
+		return head;
+	}
+}
