c6062ba044d9a47185c8b5d622ac872e5fc44dca
Nuzhat Firdosh <nuzhatfrd@gmail.com>
2025-08-28 13:02:27 +0530

    Delete node with value X in a single LL

--- LinkedList/SingleLL/deleteNodeValX.java
+++ LinkedList/SingleLL/deleteNodeValX.java
@@ -1,2 +1,22 @@
 package LinkedList.SingleLL;
 //Delete the element with value x
+class deleteNodeValX{
+	public ListNode delete(ListNode head , int X){
+		ListNode current = head;
+		ListNode prev = null;
+		if(head == null) return null;
+		if(head.val == val){
+			head = head.next;
+			return head;
+		}
+		while(current != null){
+			if(current.val == X){
+				prev.next = current.next;
+				break;
+			}
+			prev = current;
+			current = current.next;
+		}
+		return head;
+	}
+}
