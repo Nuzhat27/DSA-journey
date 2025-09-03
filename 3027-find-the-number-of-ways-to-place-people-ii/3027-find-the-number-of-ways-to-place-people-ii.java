cffb122b94981d499e001c24596b9f9f5562397b
Nuzhat firdosh <nuzhatfrd@gmail.com>
2025-09-03 08:54:49 +0530

    Time: 58 ms (95.65%), Space: 45.4 MB (52.17%) - LeetHub

--- 3027-find-the-number-of-ways-to-place-people-ii/3027-find-the-number-of-ways-to-place-people-ii.java
+++ 3027-find-the-number-of-ways-to-place-people-ii/3027-find-the-number-of-ways-to-place-people-ii.java
@@ -1,25 +1,23 @@
 class Solution {
     public int numberOfPairs(int[][] points) {
-        Arrays.sort(points , (a , b) -> {
-            if(a[0] == b[0]) return Integer.compare(b[1] , a[1]);
-            return Integer.compare(a[0] , b[0]);
-        });
         int n = points.length;
         int count = 0;
-        for(int i = 0 ; i < n ; i ++){
+        Arrays.sort(points , (a,b) -> {
+            if(a[0] == b[0])return Integer.compare(b[1] , a[1]);
+            else return Integer.compare(a[0] , b[0]);
+        });
 
-            int top = points[i][1];
+        for(int i = 0 ; i < n  ; i ++){
+            int top = points[i][1];//y-coordinate of point A
             int bot = Integer.MIN_VALUE;
 
-            for(int j = i+1 ; j < n ; j ++){
-                
+            for(int j = i + 1 ; j < n ; j ++){
                 int y = points[j][1];
-                
+
                 if(y <= top && bot < y){
                     count ++;
                     bot = y;
                 }
-                if(bot == top)break;
             }
         }
         return count;
