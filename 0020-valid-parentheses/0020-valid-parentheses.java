16e5a3f83edaf52e4b39fe89406e057ec25eb27b
Nuzhat firdosh <nuzhatfrd@gmail.com>
2025-09-05 02:00:18 +0530

    Time: 2 ms (96.63%), Space: 42 MB (36.35%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,24 +1,16 @@
 class Solution {
-    public boolean isValid(String str) {
-        int size = str.length();
-        Stack<Character> st = new Stack<>();
-        for(int i = 0 ; i < size ; i ++){
-          if(str.charAt(i) == '(' || str.charAt(i) == '{'  || str.charAt(i) == '['){
-            st.push(str.charAt(i));
-          }
-          else{
-            if(st.isEmpty())return false;
-            char ch = st.peek();
-            st.pop();
-            if(!isMatched(ch , str.charAt(i)))return false;
-          }
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        int size = s.length();
+        for(char ch : s.toCharArray()){
+            if(ch == '{' || ch == '[' || ch == '(')stack.push(ch);
+            else{
+                if(stack.isEmpty())return false;
+                char st = stack.pop();
+                if(ch == '}' && st != '{' || ch == ']' && st != '[' || ch == ')' &&
+                  st != '(')return false;
+            }
         }
-        return st.isEmpty();
-    }
-    private boolean isMatched(char st , char cl){
-      if(cl == ')' && st == '(')return true;
-      else if(cl == ']' && st == '[')return true;
-      else if(cl == '}' && st == '{')return true;
-      else return false;
+        return stack.isEmpty();
     }
 }
