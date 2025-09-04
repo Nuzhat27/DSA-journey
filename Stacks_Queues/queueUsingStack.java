43d070340f46e15e038a17e11459908bb3369edf
Nuzhat Firdosh <nuzhatfrd@gmail.com>
2025-09-05 00:41:30 +0530

    Implement queue using stack

---
+++ Stacks_Queues/queueUsingStack.java
@@ -0,0 +1,40 @@
+//Implement queue using stack
+class queueUsingStack {
+  Stack<Integer> s1;
+  Stack<Integer> s2;
+
+  public queueUsingStack() {
+    s1 = new Stack<>();
+    s2 = new Stack<>();
+  }
+
+  public void push(int x) {
+    s1.push(x);
+  }
+
+  public int pop() {
+    if (!s2.isEmpty()) {
+      return s2.pop();
+    } else {
+      while (!s1.isEmpty()) {
+        s2.push(s1.pop());
+      }
+      return s2.pop();
+    }
+  }
+
+  public int peek() {
+    if (!s2.isEmpty()) {
+      return s2.peek();
+    } else {
+      while (!s1.isEmpty()) {
+        s2.push(s1.pop());
+      }
+      return s2.peek();
+    }
+  }
+
+  public boolean isEmpty() {
+    return (s1.isEmpty() && s2.isEmpty());
+  }
+}
