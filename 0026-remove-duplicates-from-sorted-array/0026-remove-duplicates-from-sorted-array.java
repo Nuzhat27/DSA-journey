ef00a9c4db2ea9d8d16694ad339626ead45935ee
Nuzhat firdosh <nuzhatfrd@gmail.com>
2025-09-17 08:55:50 +0530

    Time: 1 ms (59.34%), Space: 45 MB (41.62%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        int l = 0 ;
+        for(int i = 1 ; i < n ; i ++){
+            if(nums[i] != nums[l]){
+                nums[++l] = nums[i];
+            }
+        }
+        return l + 1;//we have to return the k unique elements...last unique element's index is l so number of unique elements = l + 1(since array uses zero-based indexing)
+    }
+}
