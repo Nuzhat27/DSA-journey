379512a373a9b90704b3df350632323a7ef0e385
Nuzhat Firdosh <nuzhatfrd@gmail.com>
2025-10-15 18:49:55 +0530

    Deleted copy of file

--- LinkedList/sort012.java
+++ LinkedList/sort012.java
@@ -4,3 +4,64 @@ Given the head of a singly linked list consisting of only 0, 1 or 2.
 Sort the given linked list and return the head of the modified list.
 Do it in-place by changing the links between the nodes without creating new nodes.
 */
+class sort012{
+	public ListNode sort(ListNode head){
+		/*Brute Force
+		ListNode temp = head;
+		int c0 = 0 , c1 = 0 , c2 = 0;
+		while(temp != null){
+			if(temp.val == 0)c0++;
+			else if(temp.val == 1)c1++;
+			
+			temp = temp.next;
+		}
+		temp = head;
+		while(temp != null){
+			if(c0 > 0){
+				temp.val = 0;
+				c0--;
+			}
+			else if(c1 > 0){
+				temp.val = 1;
+				c1--;
+			}
+			else temp.val = 2;
+			
+			temp = temp.next;
+		}
+		return head;
+		*/
+		if(head == null || head.next == null)return head;
+		//Declaration of dummy nodes
+		ListNode zero = new ListNode(-1);
+		ListNode one = new ListNode(-1);
+		ListNode two = new ListNode(-1);
+		
+		//Storing the head positions
+		ListNode zeroHead = zero;
+		ListNode oneHead = one;
+		ListNode twoHead = two;
+		
+		ListNode temp = head;
+		
+		while(temp != null){
+			if(temp.val == 0){
+				zero.next = temp;
+				zero = zero.next;
+			}
+			else if(temp.val == 1){
+				one.next = temp;
+				one = one.next;
+			}
+			else{
+				two.next = temp;
+				two = two.next;
+			}
+			temp = temp.next;
+		}
+		zero.next = (oneHead.next != null)? oneHead.next : twoHeaad.next;
+		one.next = twoHead.next;
+		two.next = null;
+		return zeroHead.next;
+	}
+}
--- Stacks_Queues/queueLL.java
+++ Stacks_Queues/queueLL.java
@@ -1,2 +1,59 @@
 package Linkedlist;
 //Implement queue using linkedlist
+class Node{
+	int val;
+	Node next;
+	Node(int val){
+		next = null;
+		this.val = val;
+	}
+}
+class queueLL{
+	int curSize;
+	Node start , end;
+	public queueLL(){
+		start = null;
+		end = null;
+		curSize = 0;
+	}
+	public void push(int x){
+		Node temp = new Node(x);
+		if(curSize == 0 ){
+			start = temp;
+			end = temp;
+		}
+		else{
+			end.next = temp;
+			end = end.next;
+		}
+		curSize ++;
+	}
+	public int pop(){
+		if(curSize == 0){
+			System.out.println("Queue is empty.");
+			return -1;
+		}
+		Node temp = start;
+		if(curSize == 1){
+			start = null;
+			end = null;
+			return temp.val;
+		}
+		else{
+			start = start.next;
+		}
+		temp.next = null;
+		curSize --;
+		return temp.val;
+	}
+	public int peek(){
+		if(curSize == 0){
+			System.out.println("Queue is empty");
+			return -1;
+		}
+		return start.val;
+	}
+	public boolean isEmpty(){
+		return (curSize == 0);
+	}
+}
