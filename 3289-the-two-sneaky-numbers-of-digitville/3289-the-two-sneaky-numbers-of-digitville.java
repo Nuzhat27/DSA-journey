d2e9cd154686a0225c597c839c7e0af31ca985b2
Nuzhat firdosh <nuzhatfrd@gmail.com>
2025-10-31 10:37:00 +0530

    Time: 1 ms (100%), Space: 45.2 MB (7.71%) - LeetHub

--- 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.java
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.java
@@ -1,15 +1,24 @@
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        int[] ans = new int[2];
-        int ind = 0;
-        for(int i = 1 ; i < n ; i ++){
-            if(nums[i] == nums[i - 1]){
-                ans[ind ++] = nums[i];
-            }
-            if(ind == 2) break;
+        int n = nums.length - 2;
+        int xorNum = 0 , xorRange = 0;
+        for(int num : nums){
+            xorNum ^= num;
         }
-        return ans;
+        for(int i = 0 ; i < n ; i ++){
+            xorRange ^= i;
+        }
+        int xorAll = xorNum ^ xorRange;
+        int lowBit = xorAll & -xorAll;
+        int x1 = 0 , x2 = 0;
+        for(int x : nums){
+            if((x & lowBit) != 0) x1 ^= x;
+            if((x & lowBit) == 0) x2 ^= x;
+        }
+        for(int i = 0 ; i < n ; i ++){
+            if((i & lowBit) != 0) x1 ^= i;
+            if((i & lowBit) == 0) x2 ^= i;
+        }
+        return new int[]{x1 , x2};
     }
 }
