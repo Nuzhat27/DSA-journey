dd88afd201ca5df44b40b045bb61924f98c9f64a
Nuzhat Firdosh <nuzhatfrd@gmail.com>
2026-01-19 08:05:23 +0530

    Easy:- Maximum Consecutive Good Numbers

---
+++ Arrays/MaxConsecutiveGoodNum.java
@@ -0,0 +1,18 @@
+package Arrays;
+class MaxConsecutiveGoodNum {
+    public int maxConsecutiveGoodNum(int[] nums) {
+        int maxCount = 0;
+        int currentCount = 0;
+
+        for (int num : nums) {
+            if (num == 1) {
+                currentCount++;
+                maxCount = Math.max(maxCount, currentCount);
+            } else {
+                currentCount = 0;
+            }
+        }
+
+        return maxCount;
+    }
+}
