2fcadc5571da5dc5509e4a102d226d8e09c51d33
Nuzhat firdosh <nuzhatfrd@gmail.com>
2026-03-04 23:12:43 +0530

    Time: 0 ms (100%), Space: 43.7 MB (76.37%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,16 +1,14 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int p1 = m - 1;
-        int p2 = n - 1;
-        int p = m + n - 1;
+        int p1 = m - 1  , p2 = n - 1 , p = m + n - 1;
         while(p1 >= 0 && p2 >= 0){
-            if(nums1[p1] > nums2[p2]){
+            if(nums1[p1] >= nums2[p2]){
                 nums1[p] = nums1[p1];
                 p1 --;
             }
             else{
                 nums1[p] = nums2[p2];
-                p2 --;
+                p2--;
             }
             p --;
         }
@@ -19,5 +17,6 @@ class Solution {
             p2 --;
             p --;
         }
+
     }
 }
