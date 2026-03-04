2b1e1819b03cef37f09b2609c23e7579c52a1537
Nuzhat firdosh <nuzhatfrd@gmail.com>
2026-03-04 20:20:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,38 +1,36 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int n = nums.length;
+        List<Integer> result = new ArrayList<>();
         int cnt1 = 0 , cnt2 = 0 , num1 = -1 , num2 = -1;
+        int n = nums.length;
         for(int num : nums){
-            if(num == num1) {
+            if(num == num1){
                 cnt1 ++;
             }
             else if(num == num2){
                 cnt2 ++;
             }
             else if(cnt1 == 0 && num != num2){
-                cnt1 = 1;
                 num1 = num;
+                cnt1 = 1;
             }
             else if(cnt2 == 0 && num != num1){
-                cnt2 = 1;
                 num2 = num;
+                cnt2 = 1;
             }
             else{
-                cnt1 -- ;
-                cnt2 -- ;
+                cnt1 --;
+                cnt2 --;
             }
         }
-        cnt1 = 0 ;
+        cnt1 = 0;
         cnt2 = 0;
         for(int num : nums){
-            if(num == num1) cnt1 ++;
-            else if(num == num2) cnt2 ++;
+            if(num == num1)cnt1++;
+            else if(num == num2)cnt2++;
         }
-        
-        List<Integer> result = new ArrayList<>();
-        if(cnt1 > n / 3) result.add(num1);
-        if(cnt2 > n / 3) result.add(num2);
-
+        if(cnt1 > n/3) result.add(num1);
+        if(cnt2 > n/3) result.add(num2);
         return result;
     }
 }
