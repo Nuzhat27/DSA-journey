dc6fe5284942797f992b872cac30d30f88e4118f
Nuzhat firdosh <nuzhatfrd@gmail.com>
2026-03-05 11:55:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,20 +1,12 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         int n = nums.length;
-        int l = 0 , r = 0;
-        while(r < n){
-            int count = 1;
-            while(r + 1 < n && nums[r] == nums[r + 1]){
-                count += 1;
-                r += 1;
+        int k = 0;
+        for(int num : nums){
+            if(k < 2 || num != nums[k - 2]){
+                nums[k ++] = num;
             }
-            int k = Math.min(2 , count);
-            for(int j = 0 ; j < k ; j ++){
-                nums[l ++] = nums[r];
-            }
-
-            r += 1;
         }
-        return l;
+        return k;
     }
 }
