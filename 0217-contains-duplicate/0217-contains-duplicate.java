e56be7b9d30f0cba2da34efad8d77e9d466adc65
Nuzhat firdosh <nuzhatfrd@gmail.com>
2026-04-16 19:34:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,8 +1,14 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
+        /*Arrays.sort(nums);
         for(int i = 1 ; i < nums.length ; i ++){
             if(nums[i] == nums[i - 1]) return true;
+        }
+        return false; */
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0 ; i < nums.length ; i ++){
+            if(set.contains(nums[i])) return true;
+            set.add(nums[i]);
         }
         return false;
     }
