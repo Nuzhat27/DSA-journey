0c10e3a7a9e1d8ab1ca68972a5f96520aa2c29bd
Nuzhat firdosh <nuzhatfrd@gmail.com>
2026-04-17 20:30:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,28 +1,42 @@
 class Solution {
     public int[] sortArray(int[] nums) {
         int n = nums.length;
-        mergeSort(nums, 0, n - 1);
+        mergeSort(nums , 0 , n - 1);
         return nums;
     }
-
-    private void mergeSort(int[] nums , int p , int r){
-        if(p >= r) return;
-        int q = (p + r) / 2;
-        mergeSort(nums , p , q);
-        mergeSort(nums , q + 1 , r);
-        merge(nums , p , q , r);
-    }
-    private void merge(int[] nums , int left , int mid , int right){
-        int[] temp = new int[right - left + 1];
-        int i = left, j = mid + 1, k = left;
-
-        while (i <= mid && j <= right) {
-            if (nums[i] <= nums[j]) temp[k++] = nums[i++];
-            else temp[k++] = nums[j++];
+    public static void mergeSort(int[] nums , int p , int r){
+        if(p < r){
+            int q = (p + r) / 2;
+            mergeSort(nums , p , q );
+            mergeSort(nums , q + 1, r);
+            merge(nums, p , q , r);
         }
-
-        while (i <= mid) temp[k++] = nums[i++];
-        while (j <= right) temp[k++] = nums[j++];
+    }
+    public static void merge(int[] nums , int low , int mid , int high){
+        ArrayList<Integer> ans = new ArrayList<>();
+        int left = low;
+        int right = mid + 1;
+        while(left <= mid && right <= high){
+            if(nums[left] <= nums[mid]){
+                ans.add(nums[left]);
+                left ++;
+            }
+            else {
+                ans.add(nums[right]);
+                right ++;
+            }
+        }
+        while(left <= mid){
+            ans.add(nums[left]);
+            left += 1;
+        }
+        while(right <= high){
+            ans.add(nums[right]);
+            right += 1;
+        }
+        for(int i = low ; i < high ; i ++){
+            nums[low] = ans.get(i - low);
+        }
         
     }
 }
