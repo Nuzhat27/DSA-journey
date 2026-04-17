d3fa007ff9790e14700bfa80712ea611b1f58898
Nuzhat firdosh <nuzhatfrd@gmail.com>
2026-04-17 06:54:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,28 +1,15 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        // Sort the array to get the lexicographically smallest and largest strings
-        int n = strs.length;
+        String ans = "";
         Arrays.sort(strs);
-        String first = strs[0];
-        String last = strs[n-1];
-        /*int min = Math.min(first.length() , last.length());
-        String ans = "" ;
-        for(int i = 0 ; i < min ; i ++){
-            if(first.charAt(i) != last.charAt(i)){
+        int length = strs.length;
+        int n = strs[0].length();
+        for(int i = 0 ; i < n ; i ++){
+            if(strs[0].charAt(i) != strs[length - 1].charAt(i)){
+                ans = strs[0].substring(0 , i);
                 break;
-            }
-            ans += first.charAt(i);
+            } 
         }
-        return ans ;*/
-        int index = 0 ;
-        while(index < first.length()){
-            if(first.charAt(index) == last.charAt(index)){
-                index++ ;
-            }
-            else{
-                break;
-            }
-        }
-        return index == 0 ? "" : first.substring(0 , index);
+        return ans;
     }
 }
