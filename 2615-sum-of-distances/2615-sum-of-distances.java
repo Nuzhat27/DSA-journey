ff5b8cf256bc1f2ac65c3d94019275bae226a90d
Nuzhat firdosh <nuzhatfrd@gmail.com>
2026-04-23 18:51:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2615-sum-of-distances/2615-sum-of-distances.java
+++ 2615-sum-of-distances/2615-sum-of-distances.java
@@ -75,3 +75,39 @@ class Solution {
 // 2. leftSum updates at the END of each loop iteration → so during the
 //    calculation it only reflects elements before current, not including it
 */
+
+/*int n = nums.length;
+        long[] res = new long[n];
+
+        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+
+        for(int i=0;i<n;i++)
+        {
+            map.computeIfAbsent(nums[i],k->new ArrayList<>()).add(i);
+        }
+
+        for(ArrayList<Integer> list : map.values())
+        {
+            int m = list.size();
+
+            long[] prefix = new long[m];
+            prefix[0] = list.get(0);
+
+            for(int i=1;i<m;i++)
+                prefix[i] = prefix[i-1] + list.get(i);
+
+            for(int i=0;i<m;i++)
+            {
+                int idx = list.get(i);
+
+                long left = (long)i * idx - (i>0 ? prefix[i-1] : 0);
+                long right = (prefix[m-1] - prefix[i]) - (long)(m-i-1) * idx;
+
+                res[idx] = left + right;
+            }
+        }
+
+        return res;
+    }
+}
+ */
