2be3d397f629bd576e23c05d8ddc781ff3474c10
Nuzhat firdosh <nuzhatfrd@gmail.com>
2026-05-25 14:24:26 +0530

    Time: 25 ms (43.77%), Space: 48.6 MB (79.46%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,16 +1,20 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
         int n = nums.length;
-        Map<Integer,Integer> prefixSumMap = new HashMap<>();
-        prefixSumMap.put(0,1);
-
-        int curPrefixSum = 0 , subArrayCount = 0;
+        /*int[] prefixSum = new int[n];
+        prefixSum[0] = nums[0];
+        for(int i = 1 ; i < n ; i ++){
+            prefixSum[i] = prefixSum[i - 1] + nums[i];
+        }*/
+        HashMap<Integer , Integer> mpp = new HashMap<>();
+        mpp.put(0 , 1);
+        int count = 0 , curPrefixSum = 0;
         for(int i = 0 ; i < n ; i ++){
             curPrefixSum += nums[i];
             int sumToRemove = curPrefixSum - k;
-            subArrayCount += prefixSumMap.getOrDefault(sumToRemove , 0);
-            prefixSumMap.put(curPrefixSum , prefixSumMap.getOrDefault(curPrefixSum , 0 ) + 1);
-        } 
-        return subArrayCount;
+            count += mpp.getOrDefault(sumToRemove , 0);
+            mpp.put(curPrefixSum , mpp.getOrDefault(curPrefixSum , 0) + 1);
+        }
+        return count;
     }
 }
