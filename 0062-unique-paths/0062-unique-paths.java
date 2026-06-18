843c4488bd7c2409dbd12575c46b920d47f09f7b
Nuzhat <nuzhatfrd@gmail.com>
2026-06-18 10:42:00 +0530

    Time: 0 ms (100%), Space: 42 MB (81.34%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,6 +1,17 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
+        int[] aboveRow = new int[n];
+        Arrays.fill(aboveRow , 1);//There is only one way to reach the top cells of the first row
+        for(int i = 1 ; i < m ; i ++){
+            int[] currentRow = new int[n];
+            currentRow[0] = 1;//There is only one way to reach the leftmost column(by going down !!!)
+            for(int j = 1 ; j < n ; j ++){
+                currentRow[j] = aboveRow[j] + currentRow[j - 1];
+            }
+            aboveRow = currentRow;
+        }
+        return aboveRow[n - 1];
+        /*
         int[] prev = new int[n];
         for(int i = 0 ; i < m ; i ++){
             int[] temp = new int[n];
@@ -9,12 +20,13 @@ class Solution {
                     temp[j] = 1;
                     continue;
                 }
-                int up = (i > 0)?prev[j] : 0;
+                int up = (i > 0)?prev[j] : 0;//
                 int left = (j > 0)? temp[j - 1] : 0;
                 temp[j] = up + left;
             }
             prev = temp;
         }
         return prev[n - 1];
+        */
     }
 }
