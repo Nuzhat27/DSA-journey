a9fcbef27a012b907cf6ca0382b1e7a459494bc0
Nuzhat <nuzhatfrd@gmail.com>
2026-07-21 18:06:38 +0530

    Time: 30 ms (84.11%), Space: 43.6 MB (93.02%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,25 +1,19 @@
 class Solution {
     public boolean canPartition(int[] nums) {
         int total = 0;
-        for(int n : nums){
-            total += n;
+        for (int num : nums) {
+            total += num;
         }
-
-        if(total % 2 != 0){
-            //Odd total partition is impossoble
+        if (total % 2 != 0) {
             return false;
         }
 
         int target = total / 2;
-
-        //1D optimised knapsack
         boolean[] dp = new boolean[target + 1];
-        //dp[s] means is it possible to make sum s using the numbers seen so far
-        dp[0] = true; // sum zero is possoble for any number seen so far
-        for(int num : nums){
-            //go right -> left to use numbers once only - 0/1 knapsack requirement
-            for(int s = target ; s >= num ; s --){
-                dp[s] = dp[s] || dp[s - num];
+        dp[0] = true;
+        for (int num : nums) {
+            for (int sum = target; sum >= num; sum--) {
+                dp[sum] = dp[sum] || dp[sum - num];
             }
         }
         return dp[target];
