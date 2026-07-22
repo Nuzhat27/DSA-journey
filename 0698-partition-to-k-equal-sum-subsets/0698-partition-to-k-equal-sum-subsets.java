ac2aa9d58d5ae53cb59a8c000741051565032777
Nuzhat <nuzhatfrd@gmail.com>
2026-07-22 18:21:31 +0530

    Time: 1590 ms (14.03%), Space: 43.2 MB (54.68%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.java
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.java
@@ -24,35 +24,5 @@ class Solution {
         }
         return false;
     }
-        /*Arrays.sort(nums);
-        reverse(nums);
-
-        if(nums[0] > target) return false;
-        int[] bucket = new int[k];
-        return backtrack(nums, 0 , bucket, target);
-    }
-    private void reverse(int[] nums){
-        int left = 0 , right = nums.length - 1;
-        while(left < right){
-            int temp = nums[left];
-            nums[left] = nums[right];
-            nums[right] = temp;
-            left ++;
-            right --;
-        }
-    }
-    private boolean backtrack(int[] nums, int index, int[] bucket, int target){
-        if(index == nums.length){
-            return true;//Every number has been placed in the bucket
-        }
-        int n = nums[index];
-        for(int i = 0 ; i < bucket.length ; i ++){
-            if(bucket[i] + n > target)continue;
-            bucket[i] += n;
-            if(backtrack(nums, index + 1, bucket, target))return true;
-            bucket[i] -= n;
-        }
-        return false;
-    }
-    */
+        
 }
