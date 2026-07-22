5103222f636a167c117942eb8e28ace149cb3215
Nuzhat <nuzhatfrd@gmail.com>
2026-07-22 18:21:06 +0530

    Time: 1585 ms (14.41%), Space: 43.2 MB (54.68%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.java
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.java
@@ -1,11 +1,30 @@
 class Solution {
+    boolean[] visited;
     public boolean canPartitionKSubsets(int[] nums, int k) {
         int total = 0 ;
         for(int n : nums)total += n;
         if(total % k != 0 )return false;
         int target = total / k;
-        
-        Arrays.sort(nums);
+        visited = new boolean[nums.length];
+        return partition(nums, 0 , k, target, 0);
+    }
+    private boolean partition(int[] nums, int index, int k, int target, int currentSum){
+        if(k == 0)return true;
+        if(currentSum == target){
+            return partition(nums, 0 , k - 1, target, 0);
+        }
+        for(int i = index ; i < nums.length ; i ++){
+            if(!visited[i] && nums[i] + currentSum <= target){
+                visited[i] = true;
+                if(partition(nums, i + 1, k , target, currentSum + nums[i])){
+                    return true;
+                }
+                visited[i] = false;
+            }
+        }
+        return false;
+    }
+        /*Arrays.sort(nums);
         reverse(nums);
 
         if(nums[0] > target) return false;
@@ -35,4 +54,5 @@ class Solution {
         }
         return false;
     }
+    */
 }
