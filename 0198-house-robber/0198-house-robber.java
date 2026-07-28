16cd38abdce8bc446bfef7e6b0ef4bd3f14e600a
Nuzhat <nuzhatfrd@gmail.com>
2026-07-28 18:19:48 +0530

    Time: 0 ms (100%), Space: 42.5 MB (88.46%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -3,10 +3,11 @@ class Solution {
         int n = nums.length;
         if(n == 1)return nums[0];
         int[] dp = new int[n];
+        //dp[i] = maximum amount of money collected from first i houses
         dp[0] = nums[0];
-        dp[1] = Math.max(nums[0] , nums[1]);
-        for(int i = 2; i < n ; i ++){
-            dp[i] = Math.max(nums[i] + dp[i - 2] , dp[i - 1]);
+        dp[1] = Math.max(nums[0], nums[1]);
+        for(int i = 2 ; i < n ; i ++){
+            dp[i] = Math.max(dp[i - 1] , dp[i - 2] + nums[i]);
         }
         return dp[n - 1];
     }
