9c7d5d3c555bdbe587e65e888c6d94e016546fff
Nuzhat <nuzhatfrd@gmail.com>
2026-07-28 20:21:39 +0530

    Time: 0 ms (100%), Space: 42.7 MB (61.29%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -7,6 +7,24 @@ class Solution {
         return Math.max(ans1, ans2);
     }
     private int houseRob(int[] nums, int start, int end){
+        int prev1 = 0 , prev2 = 0;
+        for(int i = start ; i  <= end ; i ++){
+            int curr = Math.max(prev1 , prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+}
+    /*
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n == 1)return nums[0];
+        int ans1 = houseRob(nums, 0 , n - 2);
+        int ans2 = houseRob(nums, 1, n - 1);
+        return Math.max(ans1, ans2);
+    }
+    private int houseRob(int[] nums, int start, int end){
         int len = end - start + 1;
         if(len == 1)return nums[start];
         int[] dp = new int[len];
@@ -17,4 +35,4 @@ class Solution {
         }
         return dp[len - 1];
     }
-}
+} */
