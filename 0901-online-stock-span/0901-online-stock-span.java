467c7c5a1c78cce67d449f0aa8a98309afc3a50c
Nuzhat <nuzhatfrd@gmail.com>
2026-08-01 18:07:35 +0530

    Time: 32 ms (45.07%), Space: 54.4 MB (60.75%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.java
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -1,30 +1,25 @@
-class StockSpanner {
-    private static class Pair {
-        int price;
-        int index;
-
-        public Pair(int price, int index) {
-            this.price = price;
-            this.index = index;
-        }
+class Pair{
+    int price;
+    int span;
+    Pair(int price, int span){
+        this.price = price;
+        this.span = span;
     }
-    
+}
+class StockSpanner {
     Stack<Pair> st;
-    int ind;
-
     public StockSpanner() {
         st = new Stack<>();
-        ind = -1;
     }
     
     public int next(int price) {
-        ind = ind + 1;
+        int span = 1;
         while(!st.isEmpty() && st.peek().price <= price){
+            span += st.peek().span;
             st.pop();
         }
-        int ans = ind - ((st.isEmpty())? -1 : st.peek().index);
-        st.push(new Pair(price , ind));
-        return ans;
+        st.push(new Pair(price, span));
+        return span;
     }
 }
 
