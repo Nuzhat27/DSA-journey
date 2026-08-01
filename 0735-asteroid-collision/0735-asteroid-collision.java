2cbdb65fd6642660fa6a57849860aefdbc4ce493
Nuzhat <nuzhatfrd@gmail.com>
2026-08-01 11:01:18 +0530

    Time: 6 ms (20.84%), Space: 47.1 MB (44.6%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,26 +1,36 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        int n = asteroids.length;
-        List<Integer> st = new ArrayList<>();
-        for(int i = 0 ; i < n ; i ++){
-            //Push all right moving asteroids to the stack
-            if(asteroids[i] > 0)st.add(asteroids[i]);
+        Stack<Integer> st = new Stack<>();
+        for(int asteroid : asteroids){
+            if(asteroid > 0){
+                st.push(asteroid);
+            }
             else{
-                while(!st.isEmpty() && st.get(st.size()-1) > 0 && st.get(st.size()-1) < Math.abs(asteroids[i])){
-                    st.remove(st.size()-1);
+                while(!st.isEmpty() && st.peek() > 0 && Math.abs(asteroid) > st.peek()){
+                    st.pop();
                 }
-                if(!st.isEmpty() && st.get(st.size()-1) > 0 && st.get(st.size()-1) == Math.abs(asteroids[i])){
-                    st.remove(st.size()-1);
+                if(!st.isEmpty() && st.peek() > 0 && Math.abs(asteroid) == st.peek()){
+                    st.pop();
                 }
-                else if(st.isEmpty() || st.get(st.size()-1) < 0 ){
-                    st.add(asteroids[i]);
+                else if(st.isEmpty() || st.peek() < 0){
+                    st.push(asteroid);
                 }
             }
         }
         int[] ans = new int[st.size()];
-        for(int i = 0 ; i < st.size() ; i ++){
-            ans[i] = st.get(i);
+        for(int i = 0 ; i < ans.length  ; i ++){
+            ans[i] = st.pop();
         }
+        reverse(ans);
         return ans;
+    }
+    private void reverse(int[] arr){
+        int left = 0 , right = arr.length - 1;
+        while(left < right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left ++;right--;
+        }
     }
 }
