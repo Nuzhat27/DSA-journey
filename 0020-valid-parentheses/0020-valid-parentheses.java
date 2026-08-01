65bb6dfe875f3344d6f42872b7e39340be9c8676
Nuzhat <nuzhatfrd@gmail.com>
2026-08-01 07:04:11 +0530

    Time: 4 ms (36.92%), Space: 43.3 MB (41.54%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,52 +1,14 @@
 class Solution {
-    private boolean isMatched(char open, char close) {
-       
-        // Match
-        if((open == '(' && close == ')') ||
-           (open == '[' && close == ']') ||
-           (open == '{' && close == '}')
-        )
-            return true;
-           
-        // Mismatch
-        return false;
-    }
-
-    /* Function to check if the 
-    string is valid */
-    public boolean isValid(String str) {
-       
-        // Initialize a stack
+    public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
-       
-        // Start iterating on the string
-        for(int i=0; i < str.length(); i++) {
-           
-            // If an opening bracket is found
-            if(str.charAt(i) == '(' || str.charAt(i) == '[' || 
-               str.charAt(i) == '{') {
-               
-                // Push on top of stack
-                st.push(str.charAt(i));
-            }
-           
-            // Else if a closing bracket is found
-            else {
-                // Return false if stack is empty
-                if(st.isEmpty()) return false;
-               
-                // Get the top element from stack
-                char ch = st.peek();
-                st.pop();
-               
-                /* If the opening and closing brackets 
-                are not matched, return false */
-                if(!isMatched(ch, str.charAt(i))) return false;
+        for(Character ch : s.toCharArray()){
+            if(ch == '(' || ch == '[' || ch == '{') st.push(ch);
+            else{
+                if(st.isEmpty())return false;
+                int top = st.pop();
+                if(ch == ')' && top != '(' || ch == ']' && top != '[' || ch == '}' && top != '{')return false;
             }
         }
-       
-        /* If stack is empty, the 
-        string is valid, else invalid */
         return st.isEmpty();
     }
 }
