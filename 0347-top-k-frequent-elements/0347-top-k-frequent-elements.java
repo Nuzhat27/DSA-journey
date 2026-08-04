e032e86553a93eb439380b0e7c6a1e603b797506
Nuzhat <nuzhatfrd@gmail.com>
2026-08-04 08:53:45 +0530

    Time: 11 ms (96.22%), Space: 47.6 MB (49.78%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,27 +1,27 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        int n = nums.length;
-        HashMap<Integer , Integer> mpp = new HashMap<>();
+        HashMap<Integer, Integer> mpp = new HashMap<>();
         for(int num : nums){
-            mpp.put(num , mpp.getOrDefault(num , 0 ) + 1);
+            mpp.put(num, mpp.getOrDefault(num,0) + 1);
         }
-        List<Integer>[] freq = new List[n + 1];
-        for(int i = 0 ; i < freq.length ; i ++){
-            freq[i] = new ArrayList<>();
-        } 
-
-        for(var entry : mpp.entrySet()){
-            freq[entry.getValue()].add(entry.getKey());
+        List<Integer>[] bucket = new ArrayList[nums.length + 1];
+        for(int key : mpp.keySet()){
+            int freq = mpp.get(key);
+            if(bucket[freq] == null){
+                bucket[freq] = new ArrayList<>();
+            }
+            bucket[freq].add(key);
         }
-
-        int ind = 0 ;
-        int[] ans = new int[k];           
-        for(int i = freq.length - 1 ; i >= 0 && ind < k; i--){
-            for(int num : freq[i]){
-                ans[ind ++] = num;
-                if(ind == k) return ans;
+        int[] res = new int[k];
+        int indx = 0;
+        for(int i = bucket.length - 1 ; i >= 0 && indx < k  ; i --){
+            if(bucket[i] != null){
+                for(int num : bucket[i]){
+                    res[indx++] = num;
+                    if(indx == k)break;
+                }
             }
         }
-        return ans;
+        return res;
     }
 }
